41cc1f71bb9ee4439de1f02f57eb59869a29b8cb
Will Manson <willrtmanson@gmail.com>
2015-07-19 20:17:32 -0700

    Add playing/pausing and tidy things up

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
-var app = require('express')();
-// var events = require('events');
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var users = []
+
+app.use(express.static(__dirname + '/public'));
 
 http.listen(3000, function() {
     console.log('Listing on port 3000');
 });
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/views/index.html');
+    res.sendFile(__dirname + '/public/views/index.html');
 });
 
 io.on('connection', function(socket) {
@@ -25,5 +27,15 @@ io.on('connection', function(socket) {
 
     socket.on('add video', function(videoData) {
         io.emit('add video', videoData);
+    });
+
+    socket.on('play video', function(playData) {
+        console.log(playData.username + ' has requested to play the video');
+        io.emit('play video', playData);
+    });
+
+    socket.on('pause video', function(pauseData) {
+        console.log(pauseData.username + ' has requested to pause the video');
+        io.emit('pause video', pauseData);
     });
 });
